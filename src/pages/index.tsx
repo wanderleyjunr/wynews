ea481626bb55e9bac998e18e139fa61d8dc318d1
Wanderley Barbosa Alino Junior <wanderleyjunr@JUNR-21-FULLTRAFFIC.localdomain>
2021-06-16 08:56:38 -0300

    add tag Head file index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,16 @@
 import styled from '../styles/home.module.scss'
-
+import Head from 'next/head'
 
 export default function Home() {
   return (
-   <h1 className={styled.title}>
-     Hello <span> World</span>
+
+    <>
+    <Head>
+      <title>Home | ig.news</title>
+    </Head>
+    <h1 className={styled.title}>
+      Hello <span> World</span>
     </h1>
+    </>
   )
 }
