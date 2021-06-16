3a6c9e578879584f058cda2034d6ba1cd002ba79
Wanderley Barbosa Alino Junior <wanderleyjunr@JUNR-21-FULLTRAFFIC.localdomain>
2021-06-16 12:05:56 -0300

    criacao da page home, section hero

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
@@ -7,9 +8,18 @@ export default function Home() {
     <Head>
       <title>Home | ig.news</title>
     </Head>
-    <h1 className="">
-      Hello <span> World</span>
-    </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get acess to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
