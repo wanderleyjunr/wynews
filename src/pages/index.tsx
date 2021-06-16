6130b675f514224a6ebacba306ff5e8768bf987b
Wanderley Barbosa Alino Junior <wanderleyjunr@JUNR-21-FULLTRAFFIC.localdomain>
2021-06-16 09:36:44 -0300

    adicionando estilos globais

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import styled from '../styles/home.module.scss'
 import Head from 'next/head'
 
 export default function Home() {
@@ -8,7 +7,7 @@ export default function Home() {
     <Head>
       <title>Home | ig.news</title>
     </Head>
-    <h1 className={styled.title}>
+    <h1 className="">
       Hello <span> World</span>
     </h1>
     </>
